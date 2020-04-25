f4d4299b01069f4d51fe9c47b421a0a1e7c9aedb
hedaoos <hedaoos@oregonstate.edu>
2020-04-24 17:09:25 -0700

    Example of for loop with Finch

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+from finch import Finch
+#Here we start using the robot
+robot = Finch()
+
+notes = [880, 988, 523, 587, 659, 698]
+length = 1.2 #let's use a default length of the note
+
+for note in notes:
+    print("Note is", note)
+       
+    #Question: What happens if I *remove* the indentation for the next line?
+    robot.buzzer_with_delay(length, note)
+
