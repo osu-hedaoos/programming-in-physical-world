efdc70703d57f395acbcef9e4e790e9d3e31a2e8
hedaoos <hedaoos@oregonstate.edu>
2020-04-24 17:13:58 -0700

    While loop with robot

---
+++ while_loop.py
@@ -0,0 +1,16 @@
+from finch import Finch
+#Here we start using the robot
+robot = Finch()
+
+length = 1.2 #let's use a default length of the note
+stop_playing = False
+
+while stop_playing is False:
+    #Question: What happens if I *remove* the indentation for the next line?
+    robot.buzzer_with_delay(length, 588)
+    #pause for a minute
+    stop = input("Do you want to stop? Enter Y to stop playing. Enter anything else to continue playing")
+    if stop == 'Y':
+        stop_playing = True
+
+
