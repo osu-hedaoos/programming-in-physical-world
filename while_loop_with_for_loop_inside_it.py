a960fae669d59866d5070a12e4a9200b164b9606
hedaoos <hedaoos@oregonstate.edu>
2020-04-24 17:16:01 -0700

    Example of combining a while loop with a for loop

---
+++ while_loop_with_for_loop_inside_it.py
@@ -0,0 +1,21 @@
+from finch import Finch
+#Here we start using the robot
+robot = Finch()
+
+stop_playing = False
+
+notes = [880, 988, 523, 587, 659, 698]
+length = 1.2 #let's use a default length of the note
+
+ 
+while stop_playing is False:
+    for note in notes:
+        print("Note is", note)
+        #Question: What happens if I *remove* the indentation for the next line?
+        robot.buzzer_with_delay(length, note)
+    #pause for a minute
+    stop = input("Do you want to stop? Enter Y to stop playing. Enter anything else to continue playing")
+    if stop == 'Y':
+        stop_playing = True
+
+
